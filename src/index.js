141830ad84bd1c617ddd0094c013f8eea8928dad
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-12-04 00:28:31 +0300

    sets up bootstrap

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 import Store from './redux/ConfigureStore';
-// import fetchGreeting from './redux/greeting'
-
-// Store.dispatch(fetchGreeting());
 
 ReactDOM.render(
   <React.StrictMode>
